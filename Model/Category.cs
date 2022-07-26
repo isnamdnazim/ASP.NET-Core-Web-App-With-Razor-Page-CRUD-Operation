d1f36cacaff748213782bffa480f396345a89342
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-07-26 18:00:04 +0600

    category model created

---
+++ Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorWebApplication.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
