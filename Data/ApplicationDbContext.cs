8fd568ccd3f4a35521b880d14eac81bc218ba2ea
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-07-26 18:54:38 +0600

    migration done and database added to mssql

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorWebApplication.Model;
+
+namespace RazorWebApplication.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Category> category { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RazorWebApplication.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
